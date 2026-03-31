7c951d1805c559959b97e1f5b32e5390b148d793
luigiboy72 <swarit.madhan@gmail.com>
2026-03-31 13:57:12 +0530

    Changed server.js cuz port issues

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/feedback_system')
   .then(() => {
     console.log('Connected to MongoDB');
-    if (process.env.NODE_ENV !== 'production') {
-       app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    }
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error('MongoDB connection error:', err));
 
